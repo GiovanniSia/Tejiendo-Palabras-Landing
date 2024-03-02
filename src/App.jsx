b89e5e5e7f0803e556ae4897cc76b7f425128247
Giovanni Sia <76817363+GiovanniSia@users.noreply.github.com>
2024-03-02 16:09:40 -0300

    add main app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import "./App.css";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import MainContent from "./components/mainContent/MainContent";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="containerMain">
+        <Header />
+        <main>
+          <MainContent />
+          <Contact />
+        </main>
+        <Footer />
+      </div>
+    </>
+  );
 }
 
 export default App;
