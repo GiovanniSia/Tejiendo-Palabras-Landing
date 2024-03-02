87efca101a0a620ab7dc9b2169f8817a3cf97ba7
Giovanni Sia <76817363+GiovanniSia@users.noreply.github.com>
2024-03-02 16:10:06 -0300

    add contact with css

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,40 @@
+import "./Contact.css";
+import ImgLogoInstagram from "../../assets/logoInstagramBlue.svg";
+import ImgLogoWhatsapp from "../../assets/logoWhatsapp.svg";
+
+const Contact = () => {
+  return (
+    <div className="contact-section">
+      <h1 className="title-general">Sobre nosotros</h1>
+      <div className="contact-inside">
+        <div className="contact">
+          <h2>Contactanos</h2>
+          <div className="contact-info">
+            <div className="contact-item">
+              <img className="img-contact" src={ImgLogoInstagram} alt="" />
+              <span>tejiendo__palabras</span>
+            </div>
+            <div className="contact-item">
+              <img className="img-contact" src={ImgLogoWhatsapp} alt="" />
+              <span>+57 321 4833948</span>
+            </div>
+          </div>
+        </div>
+        <div className="delivery">
+          <h2>Métodos de entrega</h2>
+          <div className="delivery-info">
+            <div className="delivery-item">
+              <strong className="img-contact">🏡</strong>
+              <span>A domicilio en Neiva y Palermo</span>
+            </div>
+            <div className="delivery-item">
+              <strong className="img-contact">🚛</strong>
+              <span>Servientrega</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Contact;
