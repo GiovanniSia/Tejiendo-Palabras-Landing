c0843205bef1853f19a116031d1fb4d032d240e1
Giovanni Sia <76817363+GiovanniSia@users.noreply.github.com>
2024-03-02 16:10:19 -0300

    add footer with css

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,28 @@
+import "./Footer.css";
+import ImgLogoInstagram from "../../assets/logoInstagram.svg";
+import ImgLogoWhatsapp from "../../assets/logoWhatsappWhite.svg";
+const Footer = () => {
+  return (
+    <>
+      <div className="footer">
+        <div className="contact-footer">
+          <h1>Contacto</h1>
+          <div>
+            <a href="">
+              <img className="img-footer" src={ImgLogoInstagram} alt="" />
+            </a>
+            <a href="">
+              <img className="img-footer" src={ImgLogoWhatsapp} alt="" />
+            </a>
+          </div>
+        </div>
+        <div className="by-footer">
+          <p className="text-footer">
+            Hecho por &lt <a href="">Your Software</a> &gt
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Footer;
