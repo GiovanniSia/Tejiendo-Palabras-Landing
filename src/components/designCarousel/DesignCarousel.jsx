cf20b4e88b4cfb574bd4ad40ec198363611a656f
Giovanni Sia <76817363+GiovanniSia@users.noreply.github.com>
2024-03-03 13:02:14 -0300

    crochet images in .js

--- src/components/designCarousel/DesignCarousel.jsx
+++ src/components/designCarousel/DesignCarousel.jsx
@@ -1,39 +1,21 @@
 import { useState } from "react";
-import Picture1 from "../../assets/imgDesigns/picture1.jpeg";
-import Picture3 from "../../assets/imgDesigns/picture3.jpeg";
-import Picture2 from "../../assets/imgDesigns/picture2.jpeg";
-import Picture4 from "../../assets/imgDesigns/picture4.jpeg";
-import Picture5 from "../../assets/imgDesigns/picture5.jpeg";
-import Picture6 from "../../assets/imgDesigns/picture6.jpeg";
-import Picture7 from "../../assets/imgDesigns/picture7.jpeg";
-import Picture8 from "../../assets/imgDesigns/picture8.jpeg";
+import { designCrochet } from "../../service/imageLoader";
 
 const DesignCarousel = () => {
   const [currentPage, setCurrentPage] = useState(0);
   const itemsPerPage = 4;
 
-  const picturesDesigns = [
-    { id: 1, src: Picture1, alt: "Picture 1" },
-    { id: 2, src: Picture2, alt: "Picture 2" },
-    { id: 3, src: Picture3, alt: "Picture 3" },
-    { id: 4, src: Picture4, alt: "Picture 4" },
-    { id: 5, src: Picture5, alt: "Picture 5" },
-    { id: 6, src: Picture6, alt: "Picture 6" },
-    { id: 7, src: Picture7, alt: "Picture 7" },
-    { id: 8, src: Picture8, alt: "Picture 8" },
-  ];
-
   const nextPage = () => {
     setCurrentPage(
       (currentPage) =>
-        (currentPage + 1) % Math.ceil(picturesDesigns.length / itemsPerPage)
+        (currentPage + 1) % Math.ceil(designCrochet.length / itemsPerPage)
     );
   };
 
   const prevPage = () => {
     setCurrentPage((currentPage) =>
       currentPage === 0
-        ? Math.ceil(picturesDesigns.length / itemsPerPage) - 1
+        ? Math.ceil(designCrochet.length / itemsPerPage) - 1
         : currentPage - 1
     );
   };
@@ -42,7 +24,7 @@ const DesignCarousel = () => {
     <div className="designInstagram">
       <h1 className="title-general">Diseños</h1>
       <div className="feed">
-        {picturesDesigns
+        {designCrochet
           .slice(currentPage * itemsPerPage, (currentPage + 1) * itemsPerPage)
           .map((picture) => (
             <div className="foto" key={picture.id}>
---
+++ src/service/imageLoader.js
@@ -0,0 +1,43 @@
+import Picture1 from "../assets/imgDesigns/picture1.jpeg";
+import Picture3 from "../assets/imgDesigns/picture3.jpeg";
+import Picture2 from "../assets/imgDesigns/picture2.jpeg";
+import Picture4 from "../assets/imgDesigns/picture4.jpeg";
+import Picture5 from "../assets/imgDesigns/picture5.jpeg";
+import Picture6 from "../assets/imgDesigns/picture6.jpeg";
+import Picture7 from "../assets/imgDesigns/picture7.jpeg";
+import Picture8 from "../assets/imgDesigns/picture8.jpeg";
+
+export const designCrochet = [
+  {
+    src: Picture1,
+    alt: "Diseño 1",
+  },
+  {
+    src: Picture2,
+    alt: "Diseño 2",
+  },
+  {
+    src: Picture3,
+    alt: "Diseño 3",
+  },
+  {
+    src: Picture4,
+    alt: "Diseño 4",
+  },
+  {
+    src: Picture5,
+    alt: "Diseño 5",
+  },
+  {
+    src: Picture6,
+    alt: "Diseño 6",
+  },
+  {
+    src: Picture7,
+    alt: "Diseño 7",
+  },
+  {
+    src: Picture8,
+    alt: "Diseño 8",
+  },
+];
